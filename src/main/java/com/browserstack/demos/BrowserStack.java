ad1882624dad382de1dd7569d19edafaeb84374a
Varun Garg <varun.ga@browserstack.com>
2019-02-18 14:35:27 +0530

    fix desired caps
    Signed-off-by: Varun Garg <varun.ga@browserstack.com>

--- src/main/java/com/browserstack/demos/BrowserStack.java
+++ src/main/java/com/browserstack/demos/BrowserStack.java
@@ -6,7 +6,7 @@ import java.net.URL;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
 
 public class BrowserStack {
